550ca2acdf72004b5c9230bcda4f7e6b4850323b
Livia Indriana Sari <liviaindrisari@gmail.com>
2026-06-03 20:30:28 +0700

    add vercel rewrite

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: "http://localhost:3001",
-  baseURL: "http://43.157.248.229:8081",
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
