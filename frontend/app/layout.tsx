5f411de8b64880a0bfb1b28ce58955ff7f46ceb8
Livia Indriana Sari <liviaindrisari@gmail.com>
2026-06-04 10:18:02 +0700

    update

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "SIRUKA",
+  title: "SiRuka",
   icons: {
     icon: "/siruka.png",
   },
